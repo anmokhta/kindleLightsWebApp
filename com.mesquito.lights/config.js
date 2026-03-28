5dda3a3cb300f287727255e11bca7ba4a8f40ac1
Andrew Mok <amokhtarzadeh22@gmail.com>
2026-03-28 12:33:59 -0700

    config.js dummy vars

---
+++ com.mesquito.lights/config.js
@@ -0,0 +1,18 @@
+// DUMMY DATA, MUST POPULATE YOUR OWN
+
+// Can Generate from:
+// Home Assistant -> Settings -> Automations & scenes -> Create new automation 
+// Add Trigger -> Webhook
+
+
+var LOWER_LIVINGROOM_WEBHOOK = '-SomeWebhook'
+var TOGGLE_LIVINGROOM_WEBHOOK = '-SomeWebhook'
+var BRIGHTEN_LIVINGROOM_WEBHOOK = '-SomeWebhook'
+
+var LOWER_OFFICE_WEBHOOK = '-SomeWebhook'
+var TOGGLE_OFFICE_WEBHOOK = '-SomeWebhook'
+var BRIGHTEN_OFFICE_WEBHOOK = '-SomeWebhook'
+
+var LOWER_BEDROOM_WEBHOOK = '-SomeWebhook'
+var TOGGLE_BEDROOM_WEBHOOK = '-SomeWebhook'
+var BRIGHTEN_BEDROOM_WEBHOOK = '-SomeWebhook'
